a9afc192ff365cb3b01b319cc7612c61af4df800
Holli Dawson <hldawson42@gmail.com>
2023-03-07 17:03:59 -0700

    updates tests with objects and object manipulation

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Holli",
+    streetaddress: "100 Aweseome St"
+}
+
+function updateEmployeeWithKeyAndValue (employee, key, value){
+    return{
+        ...employee,
+        [key]: value
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+
+    }
+
+    function deleteFromEmployeeByKey(employee, key, value) {
+        return{
+            ...employee,
+            [key]: value
+        }
+    }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee;
+}
